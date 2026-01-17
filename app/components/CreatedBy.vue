dc48b9e20a3a5f38e0a3c345e5935e330948c0b5
ghartemann <g.hartemann@gmail.com>
2026-01-17 08:49:10 -0500

    fix

--- app/components/CreatedBy.vue
+++ app/components/CreatedBy.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="w-full text-center text-xs pb-4">
-        <div>Version {{ version }}</div>
-    <div>
-        Created by
-        <ULink to="https://ghartemann.fr" target="_blank">ghartemann</ULink>
-        © 2025
-    </div>
+        <pre class="inline">v{{ version }}</pre>
+
+        <div>
+            Created by
+            <ULink to="https://ghartemann.fr" target="_blank">ghartemann</ULink>
+            © {{ year }}
+        </div>
     </div>
 </template>
 
 <script setup>
-import v from "~/../version.json";
+import v from '~/../version.json';
 
 const version = ref(v.version);
+
+const year = new Date().getFullYear();
 </script>
 
 <style scoped>
