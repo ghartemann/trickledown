f9b38c1ac2b9a89f532d42a19b2ecb4b25ef0a97
ghartemann <g.hartemann@gmail.com>
2026-09-14 09:05:50 -0400

    commit

---
+++ app/pages/life-expectancy/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full min-h-screen flex flex-col overflow-hidden relative pb-16">
+        <AppLoader v-if="loading"></AppLoader>
+
+        <UContainer
+            v-else
+            class="mt-4 mb-8"
+        >
+            test
+        </UContainer>
+    </div>
+</template>
+
+<script setup>
+import AppLoader from '~/components/AppLoader.vue';
+
+const loading = ref(false);
+</script>
+
+<style scoped>
+
+</style>
